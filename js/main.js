21119405a4ba1f1261fa297445a0ee771d0a4bea
Dante L'Erario <dantelerario@gmail.com>
2020-04-01 16:55:28 +0200

    done

--- js/main.js
+++ js/main.js
@@ -6,9 +6,33 @@ console.log('OK');
 // per i multipli di 5 stampi Buzz.
 // per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 
-var numList = []
+// var numList = []
+//
+// for (var i = 1; i < 101; i++) {
+//
+//   if (numList[i] % 3 != 0) {
+//     var multiplo3 = 'Fizz';
+//     numList.push(multiplo3);
+//   } else {
+//     numList.push( i );
+//   }
+//   console.log(numList);
+// }
+
+var arrayNumeri = [];
 
 for (var i = 1; i < 101; i++) {
-  numList.push( i );
-  console.log(numList);
+  var nuovoNumero = i;
+
+  if (nuovoNumero % 5 == 0 && nuovoNumero % 3 == 0) {
+    arrayNumeri.push('FizzBuzz');
+  } else if (nuovoNumero % 3 == 0) {
+    arrayNumeri.push('Fizz');
+  } else if (nuovoNumero % 5 == 0) {
+    arrayNumeri.push('Buzz');
+  } else {
+    arrayNumeri.push(nuovoNumero);
+  }
+
+  console.log(arrayNumeri);
 }
